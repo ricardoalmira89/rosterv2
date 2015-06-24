b945ccac64e205c946e6e27d4ca584a1185a1979
Ricardo Almira <ricardoalmira89@bnjm.cu>
2015-06-24 14:10:16 +0200

    Generando crud de Student

---
+++ src/Alm/RosterBundle/Form/StudentType.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Alm\RosterBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class StudentType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('lastName')
+            ->add('firstName')
+            ->add('email')
+            ->add('startDate')
+            ->add('endDate')
+            ->add('homePhone')
+            ->add('bussinessPhone')
+            ->add('cellPhone')
+            ->add('faxNumber')
+            ->add('picture')
+            ->add('address')
+            ->add('city')
+            ->add('state')
+            ->add('zip')
+            ->add('country')
+            ->add('creditTrans')
+            ->add('refered')
+            ->add('webPage')
+            ->add('notes')
+            ->add('attachment')
+            ->add('location')
+            ->add('status')
+            ->add('comments')
+            ->add('emergencyContactName')
+            ->add('emergencyContactPhone1')
+            ->add('emergencyContactPhone2')
+            ->add('emergencyContactPhone3')
+            ->add('cv')
+            ->add('cvs')
+            ->add('middleInitial')
+            ->add('paymentInfo')
+            ->add('paymentPlanAmount')
+            ->add('eo')
+            ->add('graduated')
+            ->add('locker')
+            ->add('dropinfo')
+            ->add('schedules')
+            ->add('programs')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Alm\RosterBundle\Entity\Student'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'alm_rosterbundle_student';
+    }
+}
